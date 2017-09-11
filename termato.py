b6b8595d93bb741fbdf72383d9c6576029110cc4
alexpay <alexpay1999@gmail.com>
2017-09-11 09:17:58 +0100

    attempted to add keypress detection

--- termato.py
+++ termato.py
@@ -5,27 +5,34 @@ import click
 import time
 import math
 import sys
+from getch import getch
 
 #click
 @click.command()
 @click.option('-t', default=25, help='Time in minutes.')
 
-
 def timer(t):
-    start = time.monotonic()
-    duration = (t * 60)
-    for x in range(0, duration + 1):
-        while time.monotonic() - start < x:
-                pass
+	enabled = False
+	start = time.monotonic()
+	duration = (t * 60)
+	key = getch()
+	for x in range(0, duration + 1):
+		while time.monotonic() - start < x:
+			if key == 's':
+				enabled = True
+			if key == 'p':
+				enabled = False
+			if enabled == True:
+				t = (duration - x)
+				ts = ((t/60)%1)*60
+				s = int(round(ts))
+				sys.stdout.write("\033[K")
+				print ('Pomodoro Sprint:','{0:02}:{1:02}'.format(t//60 , s), end="\r")
+			else:
+				continue
 
-        t = (duration - x)
-        ts = ((t/60)%1)*60
-        s = int(round(ts))
-        sys.stdout.write("\033[K")
-        print ('Pomodoro Sprint:','{0:02}:{1:02}'.format(t//60 , s), end="\r")
-    
-    print("\n\nPomodoro Finished!\n")
+	print("\n\nPomodoro Finished!\n")
 
 if __name__ == '__main__':
-    timer()
+	timer()
         
